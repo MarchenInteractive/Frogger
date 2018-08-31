e229a4fbbab144835106842605fcdf6a97b1209c
vuraul94 <vuraul94@gmail.com>
2018-08-30 23:10:42 -0600

    adding water

--- Assets/Scripts/Frogger/MovingElement.cs
+++ Assets/Scripts/Frogger/MovingElement.cs
@@ -2,39 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingElement : MonoBehaviour {
+public class MovingElement : MonoBehaviour
+{
 
-	public string direction;
-	public float speed;
+  public string direction;
+  public float speed;
 
-	// Use this for initialization
-	void Start () {
-		StartCoroutine(Movement(direction));
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  public bool moveOther = false;
 
-	// Método corutina
-	public virtual IEnumerator Movement(string direction){
-		while(true){
-			switch(direction){
-			case "r":
-    		this.gameObject.transform.Translate(speed*Time.deltaTime,0,0);
-				break;
-			case "l":
-    		this.gameObject.transform.Translate(-speed*Time.deltaTime,0,0);
-				break;
-			case "f":
-    		this.gameObject.transform.Translate(0,0,speed*Time.deltaTime);
-				break;
-			case "b":
-    		this.gameObject.transform.Translate(0,0,-speed*Time.deltaTime);
-				break;
-		}
-    	yield return new WaitForSeconds(0.1f);
-		}	
-	}
+  private IEnumerator movementCorutine;
+
+
+  // Use this for initialization
+  void Start()
+  {
+    if (!moveOther)
+    {
+      StartCoroutine(Movement(direction, this.gameObject));
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnTriggerEnter(Collider other)
+  {
+    if (moveOther)
+    {
+      movementCorutine = Movement(direction, other.gameObject);
+      StartCoroutine(movementCorutine);
+    }
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+    if (moveOther)
+    {
+      StopCoroutine(movementCorutine);
+      movementCorutine = null;
+    }
+  }
+
+  // Método corutina
+  public virtual IEnumerator Movement(string direction, GameObject movingObject)
+  {
+    while (true)
+    {
+      switch (direction)
+      {
+        case "r":
+          movingObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+          break;
+        case "l":
+          movingObject.transform.Translate(-speed * Time.deltaTime, 0, 0);
+          break;
+        case "f":
+          movingObject.transform.Translate(0, 0, speed * Time.deltaTime);
+          break;
+        case "b":
+          movingObject.transform.Translate(0, 0, -speed * Time.deltaTime);
+          break;
+      }
+      yield return new WaitForSeconds(0.1f);
+    }
+  }
 }
--- Assets/Scripts/Frogger/Player.cs
+++ Assets/Scripts/Frogger/Player.cs
@@ -2,76 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
-	
-	// Public attributes
-	public float stepDistance = 1f;
-	public float lifes = 5f;
+public class Player : MonoBehaviour
+{
 
-	//Private attributes
-	private Vector3 _direction;
+  // Public attributes
+  public float stepDistance = 1f;
+  public float lifes = 5f;
+
+  //Private attributes
+  private Vector3 _direction;
   private bool _isReadyToMove;
   private RaycastHit _hit;
-	private Level _level;
+  private Level _level;
   private SmoothFollow _smoothFollow;
 
-	// Use this for initialization
-	void Start () {
-		_isReadyToMove = true;
-	}
-	
-	// Update is called once per frame
-	public virtual void Update () {
-		//Detect keyboard events
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-      {
-        MoveChar(Vector3.right);
-      }
-			if (Input.GetKeyDown(KeyCode.LeftArrow))
-			{
-         MoveChar(Vector3.left);
-      }
-      if (Input.GetKeyDown(KeyCode.DownArrow))
-      {
-        MoveChar(Vector3.back);
-      }
-      if (Input.GetKeyDown(KeyCode.UpArrow))
-      {
-        MoveChar(Vector3.forward);
-      }
-	}
-
-	// Fuction to move the character
-	public virtual void MoveChar(Vector3 direction) {
-		// Detect Damage
-    if(!DetectDamage(direction) && _isReadyToMove) 
-    {
-      StartCoroutine(TimedMove(direction));
-    }else{
-			this.gameObject.transform.position = new Vector3(0,0,0);
-			lifes -= 1;
-		}
+  // Use this for initialization
+  void Start()
+  {
+    _isReadyToMove = true;
   }
 
-	// Método corutina
-	public virtual IEnumerator TimedMove(Vector3 direction){
+  // Update is called once per frame
+  public virtual void Update()
+  {
+    //Detect keyboard events
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+      MoveChar(Vector3.right);
+    }
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+      MoveChar(Vector3.left);
+    }
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      MoveChar(Vector3.back);
+    }
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      MoveChar(Vector3.forward);
+    }
+  }
+
+  // Fuction to move the character
+  public virtual void MoveChar(Vector3 direction)
+  {
+    StartCoroutine(TimedMove(direction));
+  }
+
+  // Método corutina
+  public virtual IEnumerator TimedMove(Vector3 direction)
+  {
     _isReadyToMove = false;
     this.transform.Translate(direction);
     yield return new WaitForSeconds(0.1f);
     _isReadyToMove = true;
-	}
+  }
 
-	//Function that detects if a collision exist
-	public virtual bool DetectDamage(Vector3 direction){
-		// Save the data of the collision in the property _hit
-		if (Physics.Raycast(transform.position, transform.TransformDirection(direction), out _hit, 1))
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject.tag.Equals("Harmful"))
     {
-			// Detect if the collsion was with a Harmful Element
-      if(_hit.collider.gameObject.tag.Equals("Harmful")) 
-        {
-        	return true;
-        }
-		}
-		return false;
-	}
+      this.gameObject.transform.position = new Vector3(0, 0, 0);
+      lifes -= 1;
+    }
+  }
 }
